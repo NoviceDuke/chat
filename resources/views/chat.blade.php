978c53ffe720735e1c495f5b31e4f7d0e3f34a63
邱昱智 <duke@qiuyuzhide-MacBook-Pro.local>
2017-09-13 16:08:01 +0800

    1.set basic layout

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{assert('css/app.css')}}" />
+  </head>
+  <body>
+    <div class="container">
+      <div  class="row">
+          <h1>Chat Room</h1>
+      </div>
+    </div>
+  <script src="{{assert('js/app.js')}}"></script>
+  </body>
+
+</html>
