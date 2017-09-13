13c7e37efab84e737d66ad4cdad85473e0d88321
邱昱智 <duke00360753@gmail.com>
2017-09-13 17:06:23 +0800

    1.add list-group

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -10,6 +10,13 @@
     <div class="container">
       <div  class="row" id="app">
           <h1>Chat Room</h1>
+            <ul class="list-group">
+              <li class="list-group-item active">Chat room</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Morbi leo risus</li>
+              <li class="list-group-item">Porta ac consectetur ac</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
       </div>
     </div>
   <script src="{{assert('js/app.js')}}"></script>
