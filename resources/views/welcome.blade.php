6c3faae9411043c030d8064013e4d02b91a00ec3
邱昱智 <duke00360753@gmail.com>
2017-09-29 16:12:05 +0800

    1.test home view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Laravel by Docker 
                 </div>
 
                 <div class="links">
